98bfd3d48a009705465a8d06a6ec69f0f78c63c6
jun68ykt <jun68ykt@gmail.com>
2019-04-28 14:48:47 +0900

    primary.mainカラーを変更したテーマ設定

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff1493',
+    },
+  },
+});
+
 ReactDOM.render(
-  <App />,
+   <MuiThemeProvider theme={theme}>
+    <App />
+   </MuiThemeProvider>,
   document.getElementById('root')
 );
