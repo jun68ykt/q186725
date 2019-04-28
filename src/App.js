8ee29b9092cf6ccb0ef61293f32d82bc728d87e3
jun68ykt <jun68ykt@gmail.com>
2019-04-28 15:26:43 +0900

    Button追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import LabelBottomNavigation from './LabelBottomNavigation';
+import Button from '@material-ui/core/Button';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <LabelBottomNavigation />
+      <br />
+      <Button variant="contained" color="primary">
+        HELLO WORLD
+      </Button>
     </div>
   );
 }
